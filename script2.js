c39fb9be138ade19fd0c85e29697faf394f3679a
Beatriz Carvalho <beatrizcnet@gmail.com>
2020-09-16 12:42:45 -0300

    sem troca de página com o radiobuttons

---
+++ script2.js
@@ -0,0 +1,43 @@
+function mostraI() {
+  if (document.getElementById("opcaoi").checked == true){
+    document.getElementById("textoi").style.display = "block";
+  } else {
+    document.getElementById("textoi").style.display = "none";
+  }
+}
+
+function mostraR() {
+  if (document.getElementById("opcaor").checked == true){
+    document.getElementById("textor").style.display = "block";
+  } else {
+    document.getElementById("textor").style.display = "none";
+  }
+}
+
+function mostraC() {
+  if (document.getElementById("opcaoc").checked == true){
+    document.getElementById("textoc").style.display = "block";
+  } else {
+    document.getElementById("textoc").style.display = "none";
+  }
+}
+
+function calcularIMC() {
+	var pes = document.getElementById("peso").value;
+	var alt = document.getElementById("altura").value;
+	var resi = parseFloat(pes) / parseFloat(alt*alt);
+	alert ("O IMC é " + resi);
+}
+
+function calcularRetangulo() {
+	var bas = document.getElementById("base").value;
+	var altr = document.getElementById("alturar").value;
+	var resr = parseFloat(bas) * parseFloat(altr);
+	alert ("A área do retângulo é " + resr);
+}
+
+function calcularRaio() {
+	var rai = document.getElementById("raio").value;
+	var resc = 3,14 * parseFloat(rai) * parseFloat(rai);
+	alert ("A área do círculo é " + resc);
+}
